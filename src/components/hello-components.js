be668e7d5e2d03ff477257dc2b5e1fe8b183df4e
akakshuki <giangbaccai1207@gmail.com>
2021-02-20 13:10:36 +0700

    set a error

---
+++ src/components/hello-components.js
@@ -0,0 +1,14 @@
+import Component from "react";
+var hoang = "123";
+
+class helloComponents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+export default helloComponents;
