24a0b6c903bf817c60bc831e106c7db9719fe182
zovippro1996 <trunglepham1202@gmail.com>
2021-03-14 11:43:58 +0700

    Update Unit Test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  screen.debug();
   const linkElement = screen.getByText(/Hello world/i);
   expect(linkElement).toBeInTheDocument();
 });
