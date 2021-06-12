d39d1e0e2545982d8eb87c88868b8074abb4f629
zovippro1996 <trunglepham1202@gmail.com>
2021-06-13 00:32:38 +0700

    Add comment

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import React from "react";
 
 test("renders react link", async () => {
+    // Currently we have error on test in pipeline "Module history not found ...."
     render(<App />);
     const element = screen.getByText("Planthor LOGO");
     expect(element).toBeInTheDocument();
