625284680c525f8550044e60ea67e9c4a8bafb7c
zovippro1996 <trunglepham1202@gmail.com>
2021-06-13 00:43:57 +0700

    Get back UT

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import React from "react";
 
-test("renders App Compenent", async () => {
-    // // Currently we have error on test in pipeline "Module history not found ...."
-    // render(<App />);
-    // const element = screen.getByText("Planthor LOGO");
-    // expect(element).toBeInTheDocument();
+test("renders App Component", async () => {
+    // Currently we have error on test in pipeline "Module history not found ...."
+    render(<App />);
+    const element = screen.getByText("Planthor LOGO");
+    expect(element).toBeInTheDocument();
 });
