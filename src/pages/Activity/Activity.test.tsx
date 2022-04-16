a9d0c68aa5b0cca3ab88db1107054ec472fd2cb7
Luu Phat <luuphatdev@outlook.com>
2022-04-16 16:33:20 +0700

    add unit test

--- src/pages/Activity/Activity.test.tsx
+++ src/pages/Activity/Activity.test.tsx
@@ -1,7 +1,8 @@
 import "@testing-library/jest-dom";
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Activity from "./Activity";
 
 test("renders Activity page", () => {
   render(<Activity />);
+  expect(screen.getByText("This is an Activity Page"));
 });
--- src/pages/Introduction/Introduction.test.tsx
+++ src/pages/Introduction/Introduction.test.tsx
@@ -4,4 +4,5 @@ import Introduction from "./Introduction";
 
 test("renders Landing page", () => {
   render(<Introduction />);
+  expect(screen.getByText("Learn More"))
 });
