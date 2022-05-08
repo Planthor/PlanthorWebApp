5e0dec9c206655b1f88371f29346f2ca2604745b
Trung Pham <trunglepham1202@gmail.com>
2022-05-08 09:39:08 +0700

    Minor fix nav bar (#69)
    * Minor fix nav bar sticky
    
    * Create Login Modal
    
    * Remove comment out code
    
    * Fix background white typo
    
    * Remove redundant stuffs

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import logo from "../../assets/LogoPlanthor.png";
 const NavigationBar = () => {
   const listMenu = [{
@@ -24,7 +23,7 @@ const NavigationBar = () => {
   ];
 
   return (
-    <div className="bg-whie shadow-lg h-12 w-full flex items-center fixed top-0 left-0 right-0 z-50">
+    <div className="sticky bg-white shadow-lg h-12 w-full flex items-center top-0 left-0 right-0 z-50">
       <div className="container mx-auto flex justify-between">
         <div>
           <a href="/">
