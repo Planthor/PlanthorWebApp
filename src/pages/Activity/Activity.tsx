66e50b66e4d4a12fbad5a8bbb671377afe70803e
plt@netcompany.com <plt@netcompany.com>
2022-05-28 10:17:59 +0700

    Trung test 1

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -1,7 +1,8 @@
 export default function Activity() {
   return (
     <div>
-      <h1>This is an Activity Page</h1>
+      <h1>This is an Activity Page - Rebasing</h1>
+      <h2>This is an Activity H2</h2>
     </div>
   );
 }
