119f61b403b6cbb8686eafa49c84d79ebedc655c
plt@netcompany.com <plt@netcompany.com>
2022-05-28 10:20:35 +0700

    Trung add 4

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -4,6 +4,7 @@ export default function Activity() {
       <h1>This is an Activity Page - Rebasing</h1>
       <h2>This is an Activity H2</h2>
       <h3>This is an Activity H3</h3>
+      <h4>This is an Activity H4</h4>
     </div>
   );
 }
