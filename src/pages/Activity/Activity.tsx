0bf2b8e3a9f4f7f71eea3b48b385763a11a378a7
plt@netcompany.com <plt@netcompany.com>
2022-05-28 10:30:44 +0700

    Clear 1

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -1,12 +1,7 @@
 export default function Activity() {
   return (
     <div>
-      <h1>This is an Activity Page - Rebasing</h1>
-      <h2>This is an Activity H2</h2>
-      <p>This is an Trung Paragraph</p>
-      <h3>This is an Activity H3</h3>
-      <h4>This is an Activity H4</h4>
-      <p>This is an Trung Paragraph</p>
+      <h1>This is an Activity</h1>
     </div>
   );
 }
