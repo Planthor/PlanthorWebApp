762b317f792de327829b60e526eded56bd00b0fe
plt@netcompany.com <plt@netcompany.com>
2022-05-28 10:32:54 +0700

    Trung add 2

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -3,6 +3,7 @@ export default function Activity() {
     <div>
       <h1>This is an Activity Page - Rebasing</h1>
       <h2>This is an Activity H2</h2>
+      <p>This is an Trung Paragraph</p>
       <h3>This is an Activity H3</h3>
       <h4>This is an Activity H4</h4>
       <p>This is an Trung Paragraph</p>
