51cbe73e48d24fc67a321d523ad139b126220f9c
Hoang Le <le.dinhhoang.1207@outlook.com>
2022-09-17 14:54:22 +0700

    set test case for pass before merge

--- src/components/LoginModal/LoginModal.test.tsx
+++ src/components/LoginModal/LoginModal.test.tsx
@@ -1,8 +1,11 @@
 import "@testing-library/jest-dom";
 import {render, screen} from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import LoginModal from "./LoginModal";
 
 test("renders Login Modal", () => {
-  render(<LoginModal />);
+  render( <MemoryRouter>
+    <LoginModal />
+  </MemoryRouter>);
   expect(screen.getByText("Sign in to Planthor"));
 });
