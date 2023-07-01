7a4bc4f8d0afb53ee0e10b6e6a2fe9c3192d4627
Luu Phat <luuphatdev@outlook.com>
2023-07-01 16:02:22 +0700

    remove empty tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<script>
-</script>
-
 <svelte:head>
   <title>Home</title>
   <meta name="description" content="Svelte demo app" />
@@ -10,5 +7,3 @@
   <!-- <h1>This is home page</h1> -->
 </section>
 
-<style>
-</style>
