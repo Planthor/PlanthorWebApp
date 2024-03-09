4c94245cc33a7bd3826309d30cb25cc40410e592
Luu Phat <luuphatdev@outlook.com>
2024-03-09 21:16:07 +0700

    feat: init new flow login

---
+++ src/routes/api/auth/login/+server.ts
@@ -0,0 +1,23 @@
+import { redirect, type RequestHandler } from "@sveltejs/kit";
+import pkce from "pkce-gen";
+
+const generateRandomString = (length: number) => {
+  let randomString = "";
+  const possibleChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    randomString += possibleChars.charAt(
+      Math.floor(Math.random() * possibleChars.length)
+    );
+  }
+  return randomString;
+};
+
+const state = generateRandomString(16);
+const challenge = pkce.create();
+
+export const GET: RequestHandler= ({cookies})=>{
+  throw redirect(307,"")
+
+}
