f3c6b9f4ab7b36d03ddadd3148e5a1d4bb44dbcf
Luu Phat <luuphatdev@outlook.com>
2024-09-07 16:02:54 +0700

    feat: create crud ui

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Our Products</h1>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import { goto } from "$app/navigation";
+</script>
+
+<div class="mx-auto w-2/4 min-w-0 lg:py-8">
+  <Button on:click={() => goto("/products/create")}>Create Goal</Button>
+</div>
