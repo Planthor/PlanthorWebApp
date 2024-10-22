5dcd36d26b77555e9551a35ab83d7dfd0905127e
APAC\DIP4HC <dip4hc@vn.bosch.com>
2024-10-22 10:12:57 +0700

    chore: implementing

--- src/routes/settings/schema.ts
+++ src/routes/settings/schema.ts
@@ -4,7 +4,12 @@ export const formSchema = z.object({
   goalname: z.string().min(2, { message: "reqired" }).max(256),
   duedate: z
     .string()
-    .refine((v) => v, { message: "A date of due date is required!" })
+    .refine((v) => v, { message: "A date of due date is required!" }),
+  goaltype: z.string(),
+  goalcurrent: z.string().min(2, { message: "reqired" }).max(256),
+  goaltarget: z.string().min(2, { message: "reqired" }).max(256),
+  goalunit: z.string().min(2, { message: "reqired" }).max(256),
+  description: z.string().max(256)
 });
 
 export type FormSchema = typeof formSchema;
